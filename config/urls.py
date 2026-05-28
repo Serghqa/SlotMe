97f078f9036ed2f44f02d446e771cae893c1ecb2
Sergey Ponomarev <psl.ru@mail.ru>
2026-05-28 13:22:27 +0500

    templates added base.html, home.html, connected Bootstrap

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path
+from apps.core.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
 ]
 
 # if settings.DEBUG:
