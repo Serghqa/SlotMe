5a6679dab2c1cdb428dd706c51756ae75ec50d46
Sergey Ponomarev <psl.ru@mail.ru>
2026-06-12 14:03:00 +0500

    Add filter 'masters' for ServiceAdmin

--- apps/services/admin.py
+++ apps/services/admin.py
@@ -5,7 +5,7 @@ from .models import Service
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'duration', 'is_active')
-    list_filter = ('is_active',)
+    list_filter = ('is_active', 'masters')
     list_per_page = 15
     search_fields = ('name', 'description')
     list_editable = ('is_active',)
