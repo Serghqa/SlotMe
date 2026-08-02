69e6d1edc5d79f2e4315d0ff896b195584f311eb
Sergey Ponomarev <psl.ru@mail.ru>
2026-08-02 17:57:07 +0500

    Add pagination for services

--- apps/services/views.py
+++ apps/services/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from apps.appointments.services import get_paginated_page
 from .models import Service
 
 
 def service_list_view(request):
-    services = Service.objects.filter(is_active=True)
-    return render(request, 'services/service_list.html', {'services': services})
+    services_queryset = Service.objects.filter(is_active=True).order_by('price')
+    page = request.GET.get('page', 1)
+    services_page = get_paginated_page(services_queryset, page, 5)
+
+    return render(request, 'services/service_list.html', {'services': services_page})
