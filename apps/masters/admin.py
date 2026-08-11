6297b5a7e3544ce4940ce63768cd5955dbb7d590
Sergey Ponomarev <psl.ru@mail.ru>
2026-08-11 16:44:31 +0500

    Staff can't be a master

--- apps/masters/admin.py
+++ apps/masters/admin.py
@@ -39,7 +39,7 @@ class MasterAdmin(admin.ModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         """Показывает в выпадающем списке только пользователей без привязки к мастеру."""
         if db_field.name == "user":
-            kwargs["queryset"] = User.objects.filter(master_profile__isnull=True)
+            kwargs["queryset"] = User.objects.filter(master_profile__isnull=True, is_superuser=False, is_staff=False)
 
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
