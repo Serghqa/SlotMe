db2a4c60513d3e0c320d1a03437be40957fddfae
Sergey Ponomarev <psl.ru@mail.ru>
2026-08-23 13:24:14 +0500

    Add CSRF_TRUSTED_ORIGINS

--- config/settings/development.py
+++ config/settings/development.py
@@ -3,6 +3,12 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+CSRF_TRUSTED_ORIGINS = [
+    'http://192.168.9.98:8000',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+    'http://192.168.9.1:8000',
+]
 
 # Django Debug Toolbar
 INSTALLED_APPS += ['debug_toolbar']
